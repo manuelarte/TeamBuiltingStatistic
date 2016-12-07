199dc017c4259ef842a80a1a3e932d0e72c9fedf
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2016-12-07 22:48:13 +0100

    TeamBuiltingStatistic: Initial commit

---
+++ src/main/java/org/manuel/teambuilting/statistic/TeamBuiltingStatisticApplication.java
@@ -0,0 +1,18 @@
+package org.manuel.teambuilting.statistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Manuel Doncel Martos
+ * @since 07/12/2016.
+ */
+@SpringBootApplication
+public class TeamBuiltingStatisticApplication {
+
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TeamBuiltingStatisticApplication.class, args);
+    }
+
+}
