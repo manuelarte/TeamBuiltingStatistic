141674b4837847717ca6381123d70ad273a4079f
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-03-13 18:29:45 +0100

    TeamBuiltingStatistics: Fixing build.gradle, not compile

--- src/integrationTest/java/org/manuel/teambuilting/statistics/listeners/PlayerListenerTest.java
+++ src/integrationTest/java/org/manuel/teambuilting/statistics/listeners/PlayerListenerTest.java
@@ -1,21 +1,5 @@
 package org.manuel.teambuilting.statistics.listeners;
 
-import javax.inject.Inject;
-
-import org.manuel.teambuilting.statistics.messages.PlayerDeletedMessage;
-import org.manuel.teambuilting.statistics.messages.PlayerVisitedMessage;
-import org.manuel.teambuilting.statistics.player.PlayerStatisticCommandService;
-import org.manuel.teambuilting.statistics.player.PlayerStatisticRepository;
-import org.manuel.teambuilting.statistics.player.PlayerVisits;
-import org.manuel.teambuilting.statistics.player.PlayerVisitsRepository;
-import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
-
 /**
  * @author manuel.doncel.martos
  * @since 16-12-2016
