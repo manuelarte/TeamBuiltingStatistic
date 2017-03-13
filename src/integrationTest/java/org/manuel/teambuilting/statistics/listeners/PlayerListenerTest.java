e402531c5282e7de8dc79a6c77d0d3159f06ac38
Manuel Doncel Martos <manuel.doncel.martos@powerhouse.nl>
2017-03-13 19:51:47 +0100

    TeamBuiltingStatistics: Fixing build.gradle

--- src/integrationTest/java/org/manuel/teambuilting/statistics/listeners/PlayerListenerTest.java
+++ src/integrationTest/java/org/manuel/teambuilting/statistics/listeners/PlayerListenerTest.java
@@ -1,5 +1,17 @@
 package org.manuel.teambuilting.statistics.listeners;
 
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.manuel.teambuilting.statistics.messages.Player;
+import org.manuel.teambuilting.statistics.player.PlayerStatisticRepository;
+import org.manuel.teambuilting.statistics.player.PlayerVisitsRepository;
+import org.springframework.amqp.rabbit.test.RabbitListenerTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author manuel.doncel.martos
  * @since 16-12-2016
